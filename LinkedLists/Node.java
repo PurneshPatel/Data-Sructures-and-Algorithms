8a933ba036fc3f6754c2c59af22269d0875798a8
PurneshPatel <84800964+PurneshPatel@users.noreply.github.com>
2021-08-08 22:54:14 +0530

    Add files via upload

---
+++ LinkedLists/Node.java
@@ -0,0 +1,11 @@
+package LinkedLists;
+
+public class Node {
+    public int data;
+    public Node next;
+    public Node prev;
+ 
+    public void displayNodeData() {
+        System.out.println("{ " + data + " } ");
+    }
+}
